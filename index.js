9dc79a04e577557548e546762cdb6954f4af8e83
Martin Tapia <martin@phantombuster.com>
2021-01-23 18:20:00 +0100

    Log build progress on stdout

--- index.js
+++ index.js
@@ -5,21 +5,29 @@ const showdown = require("showdown")
 const templates = {}
 for (const templateFilePath of glob.sync("templates/**/*.html")) {
 	const templateName = templateFilePath.replace(/^templates\//, "").replace(/\.html$/, "")
+	console.log(`${templateFilePath} → ${templateName}`)
 	templates[templateName] = fs.readFileSync(templateFilePath).toString()
 }
+console.log("")
 
 const converter = new showdown.Converter({
 	metadata: true,
 })
 
 for (const mdFilePath of glob.sync("dist/**/*.md")) {
+	console.log(`* In: ${mdFilePath}`)
 	const html = converter.makeHtml(fs.readFileSync(mdFilePath).toString())
 	const metadata = converter.getMetadata()
 	const outputFilePath = mdFilePath.replace(/\.md$/, ".html")
+	console.log(`* Out: ${outputFilePath}`)
 	let output = templates[metadata.template].replace("{{markdown}}", html)
 	for (const match of output.match(/({{.*?}})/g)) {
 		const key = match.replace("{{", "").replace("}}", "")
 		output = output.replace(match, metadata[key])
+		if (!metadata[key]) {
+			console.log(`${key} is ${metadata[key]}`)
+		}
 	}
 	fs.writeFileSync(outputFilePath, output)
+	console.log("")
 }
